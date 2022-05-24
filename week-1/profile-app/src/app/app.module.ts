e19b9869e69bc6afb16087abccd79aa4f18fdc36
skerrey <sethkerrey@gmail.com>
2022-05-24 13:40:41 -0500

    my-image my-details componenets

--- week-1/profile-app/src/app/app.module.ts
+++ week-1/profile-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyImageComponent } from './my-image/my-image.component';
+import { MyDetailsComponent } from './my-details/my-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyImageComponent,
+    MyDetailsComponent
   ],
   imports: [
     BrowserModule
