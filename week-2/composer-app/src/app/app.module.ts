1a74f6fe3b51b7225fd23dce307efc9c77ee0a53
skerrey <sethkerrey@gmail.com>
2022-06-05 09:54:50 -0500

    create composer-list component

--- week-2/composer-app/src/app/app.module.ts
+++ week-2/composer-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComposerListComponent } from './composer-list/composer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComposerListComponent
   ],
   imports: [
     BrowserModule,
