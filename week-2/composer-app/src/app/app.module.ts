9b9035427f008796530cea0bdb6fbdeabd0fe516
skerrey <sethkerrey@gmail.com>
2022-06-05 09:55:09 -0500

    Create contact component

--- week-2/composer-app/src/app/app.module.ts
+++ week-2/composer-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComposerListComponent } from './composer-list/composer-list.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComposerListComponent
+    ComposerListComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
