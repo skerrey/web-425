656515e85df48b840f9be2d713d6151f98453b2f
skerrey <sethkerrey@gmail.com>
2022-06-09 10:53:41 -0500

    exercise 2.3

--- week-3/enhanced-composer-app/src/app/composer-list/composer-list.component.ts
+++ week-3/enhanced-composer-app/src/app/composer-list/composer-list.component.ts
@@ -1,12 +1,12 @@
 /**
 ============================================
-; Title: Assignment 2.4 - Routing in Action
+; Title: Exercise 3.2 - Passing Data to Routes, Part 1
 ; File Name: composer-list.component.ts
 ; Author: Professor Krasso
-; Date: 5 June 2022
+; Date: 12 June 2022
 ; Modified By: Seth Kerrey
 ; Description:
-;   composer-list component TypeScript for composer-app
+;   composer-list component TypeScript for enhanced-composer-app
 ; Resources:
 ;   buwebdev, Professor Krasso, Bellevue University
 ;   Stack Overflow Array in Angular
@@ -15,27 +15,8 @@
 */
 
 import { Component, OnInit } from '@angular/core';
-
-// person object
-export default class Composer {
-  // local variables
-  fullName: string;
-  genre: string;
-  number: number;
-
-  // passes in local variables, instantiates class
-  constructor(number: number, fullName: string, genre: string) {
-    this.fullName = fullName;
-    this.genre = genre;
-    this.number = number;
-  }
-
-  // test variables are added correctly
-  toString() {
-    console.log(`\n  Full name: ${this.fullName}\n  Genre: ${this.genre}`);
-  }
-}
-
+import { IComposer } from '../composer.interface';
+import { Composer } from '../composer.class';
 
 @Component({
   selector: 'app-composer-list',
@@ -44,17 +25,10 @@ export default class Composer {
 })
 export class ComposerListComponent implements OnInit {
 
-  composers: Array<Composer>; // Composers array
+  composers: Array<IComposer>;
 
   constructor() {
-    this.composers = [ // initialize new Composer obj
-      new Composer(1, "Ludwig van Beethoven", "Classical"),
-      new Composer(2, "Wolfgang Amadeus", "Classical, Children's"),
-      new Composer(3, "Johann Sebastian Bach", "Classical"),
-      new Composer(4, "Frederic Chopin", "Classical, Jazz"),
-      new Composer(5, "Johannes Brahms", "Classical")
-    ]
-    this.composers.toString();
+    this.composers = new Composer().getComposers();
    }
 
   ngOnInit(): void {
