d8c1dc20f62eb257833c7affafcf0c37b7b22a68
skerrey <sethkerrey@gmail.com>
2022-06-11 13:13:44 -0500

    Update home.component.ts

--- week-3/secure-profile-app/src/app/home/home.component.ts
+++ week-3/secure-profile-app/src/app/home/home.component.ts
@@ -1,4 +1,19 @@
+/**
+============================================
+; Title: Exercise 3.3 - Passing Data to Routes, Part 2
+; File Name: app.component.ts
+; Author: Professor Krasso
+; Date: 12 June 2022
+; Modified By: Seth Kerrey
+; Description:
+;   home component TypeScript for secure-profile-app
+; Resources:
+;   buwebdev, Professor Krasso, Bellevue University
+===========================================
+*/
+
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +22,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn: Boolean;
+
+  constructor(private route: ActivatedRoute) { // passing login info values
+    this.isLoggedIn = Boolean(this.route.snapshot.queryParamMap.get('isLoggedIn'));
+   }
 
   ngOnInit(): void {
   }
