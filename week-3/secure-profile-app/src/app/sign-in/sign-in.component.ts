c6ff473adc0a6c958aa015bd59c9b1fde219b852
skerrey <sethkerrey@gmail.com>
2022-06-11 13:13:54 -0500

    Update sign-in.component.ts

--- week-3/secure-profile-app/src/app/sign-in/sign-in.component.ts
+++ week-3/secure-profile-app/src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,19 @@
+/**
+============================================
+; Title: Exercise 3.3 - Passing Data to Routes, Part 2
+; File Name: sign-in.component.ts
+; Author: Professor Krasso
+; Date: 12 June 2022
+; Modified By: Seth Kerrey
+; Description:
+;   sign-in component TypeScript for secure-profile-app
+; Resources:
+;   buwebdev, Professor Krasso, Bellevue University
+===========================================
+*/
+
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +22,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn = false;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  signin() { // sign in form & validate if logged in
+    this.isLoggedIn = true;
+
+    this.router.navigate(['/home'], {queryParams: {isLoggedIn: this.isLoggedIn}, skipLocationChange: true})
   }
 
 }
