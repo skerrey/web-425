142716bacfd37e729b0abf7f8d9bfa01f5c48f84
skerrey <sethkerrey@gmail.com>
2022-06-27 16:17:34 -0500

    add components

--- week-6/gpa-calculator-app1/src/app/app.module.ts
+++ week-6/gpa-calculator-app1/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { BaseLayoutComponent } from './base-layout/base-layout.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthLayoutComponent,
+    BaseLayoutComponent,
+    NotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
